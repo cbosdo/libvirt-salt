037b73bdfb81702670cebe254c1608eafd712d97
Cédric Bosdonnat <cbosdonnat@suse.com>
2018-03-09 13:51:16 +0100

    Add domain and network callbacks

--- libvirt_events.py
+++ libvirt_events.py
@@ -163,6 +163,199 @@ def domainEventLifecycleCallback (conn, dom, event, detail, opaque):
     })
 
 
+def domainEventRebootCallback(conn, dom, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], { })
+
+
+def domainEventRTCChangeCallback(conn, dom, utcoffset, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'utcoffset': utcoffset
+    })
+
+
+def domainEventWatchdogCallback(conn, dom, action, opaque):
+    actions = ( 'none', 'pause', 'reset', 'poweroff', 'shutdown', 'debug', 'inject NMI' )
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'action': nth(actions, action, 'unknown')
+    })
+
+
+def domainEventIOErrorReasonCallback(conn, dom, srcpath, devalias, action, reason, opaque):
+    actions = ( 'none', 'pause', 'report' )
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'srcPath': srcpath,
+        'devAlias': devalias,
+        'action': nth(actions, action, 'unknown'),
+        'reason': reason
+    })
+
+
+def domainEventGraphicsCallback(conn, dom, phase, localAddr, remoteAddr, authScheme, subject, opaque):
+    phases = ( 'connect', 'initialize', 'disconnect' )
+    families = ( 'ipv4', 'ipv6', 'unix' )
+
+    def getAddress(addr):
+        data = { 'family': nth(families, addr.family, 'unknown'),
+                 'node': addr.node }
+        if addr.service is not None:
+            data['service'] = addr.service
+        return addr
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'phase': nth(phases, phase, 'unknown'),
+        'local': getAddress(localAddr),
+        'remote': getAddress(remoteAddr),
+        'authScheme': authScheme,
+        'subject': {
+            'type': subject.type,
+            'name': subject.name
+        }
+    })
+
+
+def domainEventControlErrorCallback(conn, dom, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {})
+
+
+def domainEventDiskChangeCallback(conn, dom, oldSrcPath, newSrcPath, devAlias, reason, opaque):
+    reasons = ( 'change missing on start', 'drop missing on start' )
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'oldSrcPath': oldSrcPath,
+        'newSrcPath': newSrcPath,
+        'devAlias': devAlias,
+        'reason': nth(reasons, reason, 'unknown')
+    })
+
+
+def domainEventTrayChangeCallback(conn, dom, devAlias, reason, opaque):
+    reasons = ( 'open', 'close' )
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'devAlias': devAlias,
+        'reason': nth(reasons, reason, 'unknown')
+    })
+
+
+def domainEventPMWakeupCallback(conn, dom, reason, opaque):
+    reasons = ( )  # Currently unused
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'reason': nth(reasons, reason, 'unknown')
+    })
+
+
+def domainEventPMSuspendCallback(conn, dom, reason, opaque):
+    reasons = ( )  # Currently unused
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'reason': nth(reasons, reason, 'unknown')
+    })
+
+
+def domainEventBalloonChangeCallback(conn, dom, actual, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'actual': actual
+    })
+
+
+def domainEventPMSuspendDiskCallback(conn, dom, reason, opaque):
+    reasons = ( )  # Currently unused
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'reason': nth(reasons, reason, 'unknown')
+    })
+
+
+def domainEventBlockJobCallback(conn, dom, disk, job_type, status, opaque):
+    types = ( 'unknown', 'pull', 'copy', 'commit', 'active commit' )
+    statuses = ( 'completed', 'failed', 'canceled', 'ready' )
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'disk': disk,
+        'type': nth(types, job_type, 'unknown'),
+        'status': nth(statuses, status, 'unknown')
+    })
+
+
+def domainEventDeviceRemovedCallback(conn, dom, dev, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'dev': dev
+    })
+
+
+def domainEventTunableCallback(conn, dom, params, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'params': params
+    })
+
+
+def domainEventAgentLifecycleCallback(conn, dom, state, reason, opaque):
+    states = ( 'connected', 'disconnected' )
+    reasons = ( 'unknown', 'domain started', 'channel' )
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'state': nth(states, state, 'unknown'),
+        'reason': nth(reasons, reason, 'unknown')
+    })
+
+
+def domainEventDeviceAddedCallback(conn, dom, dev, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'dev': dev
+    })
+
+
+def domainEventMigrationIteration(conn, dom, iteration, opaque):
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'iteration': iteration
+    })
+
+
+def domainEventJobCompletedCallback(conn, dom, params, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'params': params
+    })
+
+
+def domainEventDeviceRemovalFailedCallback(conn, dom, dev, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'dev': dev
+    })
+
+
+def domainEventMetadataChangeCallback(conn, dom, mtype, nsuri, opaque):
+    types = ( 'description', 'title', 'element' )
+
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'type': nth(types, mtype, 'unknown'),
+        'nsuri': nsuri
+    })
+
+
+def domainEventBlockThresholdCallback(conn, dom, dev, path, threshold, excess, opaque):
+    saltSendDomainEvent(opaque, conn, dom, opaque['event'], {
+        'dev': dev,
+        'path': path,
+        'threshold': threshold,
+        'excess': excess
+    })
+
+
+def networkEventLifecycleCallback(conn, net, event, detail, opaque):
+    events = ( 'defined', 'undefined', 'started', 'stopped' )
+    details = ( 'added', 'removed', 'started', 'stopped' )
+
+    saltSendEvent(opaque, conn, {
+        'network': net.name(),
+        'event': nth(events, event, 'unknown'),
+        'detail': nth(details, detail, 'unknown'),
+    })
+
+
 def start(uri="qemu:///system",
           tag_prefix="salt/engines/libvirt_events",
           filters=["all"]):
@@ -184,6 +377,11 @@ def start(uri="qemu:///system",
                 'register': 'domainEventRegisterAny',
                 'deregister': 'domainEventDeregisterAny'
             },
+            'network': {
+                'callbacks': { },
+                'register': 'networkEventRegisterAny',
+                'deregister': 'networkEventDeregisterAny'
+            }
         }
 
         def addCallback(obj, event, event_id, callback):
@@ -198,8 +396,92 @@ def start(uri="qemu:///system",
                 log.warn('Skip "%s/%s" events: libvirt too old' % (obj, event))
 
         addCallback('domain', 'lifecycle',
-                    "VIR_DOMAIN_EVENT_ID_LIFECYCLE",
+                    'VIR_DOMAIN_EVENT_ID_LIFECYCLE',
                     domainEventLifecycleCallback)
+        addCallback('domain', 'reboot',
+                    'VIR_DOMAIN_EVENT_ID_REBOOT',
+                    domainEventRebootCallback)
+        addCallback('domain', 'rtcchange',
+                    'VIR_DOMAIN_EVENT_ID_RTC_CHANGE',
+                    domainEventRTCChangeCallback)
+        addCallback('domain', 'watchdog',
+                    'VIR_DOMAIN_EVENT_ID_WATCHDOG',
+                    domainEventWatchdogCallback)
+        addCallback('domain', 'graphics',
+                    'VIR_DOMAIN_EVENT_ID_GRAPHICS',
+                    domainEventGraphicsCallback)
+        addCallback('domain', 'ioerror',
+                    'VIR_DOMAIN_EVENT_ID_IO_ERROR_REASON',
+                    domainEventIOErrorReasonCallback)
+        addCallback('domain', 'control error',
+                    'VIR_DOMAIN_EVENT_ID_CONTROL_ERROR',
+                    domainEventControlErrorCallback)
+        addCallback('domain', 'disk change',
+                    'VIR_DOMAIN_EVENT_ID_DISK_CHANGE',
+                    domainEventDiskChangeCallback)
+        addCallback('domain', 'tray change',
+                    'VIR_DOMAIN_EVENT_ID_TRAY_CHANGE',
+                    domainEventTrayChangeCallback)
+        addCallback('domain', 'pmwakeup',
+                    'VIR_DOMAIN_EVENT_ID_PMWAKEUP',
+                    domainEventPMWakeupCallback)
+        addCallback('domain', 'pmsuspend',
+                    'VIR_DOMAIN_EVENT_ID_PMSUSPEND',
+                    domainEventPMSuspendCallback)
+        addCallback('domain', 'balloon change',
+                    'VIR_DOMAIN_EVENT_ID_BALLOON_CHANGE',
+                    domainEventBalloonChangeCallback)
+        addCallback('domain', 'pmsuspenddisk',
+                    'VIR_DOMAIN_EVENT_ID_PMSUSPEND_DISK',
+                    domainEventPMSuspendDiskCallback)
+
+        # Handle either BLOCK_JOB or BLOCK_JOB_2, but prefer the latter
+        try:
+            blockJobId = libvirt.VIR_DOMAIN_EVENT_ID_BLOCK_JOB_2
+        except AttributeError:
+            blockJobId = libvirt.VIR_DOMAIN_EVENT_ID_BLOCK_JOB
+        addCallback('domain', 'block job', blockJobId,
+                    domainEventBlockJobCallback)
+
+        addCallback('domain', 'device removed',
+                    'VIR_DOMAIN_EVENT_ID_DEVICE_REMOVED',
+                    domainEventDeviceRemovedCallback)
+
+        addCallback('domain', 'tunable',
+                    'VIR_DOMAIN_EVENT_ID_TUNABLE',
+                    domainEventTunableCallback)
+
+        addCallback('domain', 'agent lifecycle',
+                    'VIR_DOMAIN_EVENT_ID_AGENT_LIFECYCLE',
+                    domainEventAgentLifecycleCallback)
+
+        addCallback('domain', 'device added',
+                    'VIR_DOMAIN_EVENT_ID_DEVICE_ADDED',
+                    domainEventDeviceAddedCallback)
+
+        addCallback('domain', 'migration iteration',
+                    'VIR_DOMAIN_EVENT_ID_MIGRATION_ITERATION',
+                    domainEventMigrationIteration)
+
+        addCallback('domain', 'job completed',
+                    'VIR_DOMAIN_EVENT_ID_JOB_COMPLETED',
+                    domainEventJobCompletedCallback)
+
+        addCallback('domain', 'device removal failed',
+                    'VIR_DOMAIN_EVENT_ID_DEVICE_REMOVAL_FAILED',
+                    domainEventDeviceRemovalFailedCallback)
+
+        addCallback('domain', 'event metadata change',
+                    'VIR_DOMAIN_EVENT_ID_METADATA_CHANGE',
+                    domainEventMetadataChangeCallback)
+
+        addCallback('domain', 'block threshold',
+                    'VIR_DOMAIN_EVENT_ID_BLOCK_THRESHOLD',
+                    domainEventBlockThresholdCallback)
+
+        addCallback('network', 'lifecycle',
+                    'VIR_NETWORK_EVENT_ID_LIFECYCLE',
+                    networkEventLifecycleCallback)
 
         # TODO Add more callbacks
 
